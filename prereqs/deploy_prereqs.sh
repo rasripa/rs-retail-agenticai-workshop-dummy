e19c9d96348ac1ea7d003fcbe93824b0b4aa37d5
Rajesh S <rajesh.sripathi2@gmail.com>
2025-09-10 16:51:48 +0100

    Update deploy_prereqs.sh

--- prereqs/deploy_prereqs.sh
+++ prereqs/deploy_prereqs.sh
@@ -4,12 +4,12 @@
 #python knowledge_base.py --mode create
 
 # create required dynamodb tables
-pip install dynamodb-json -q
+pip3 install dynamodb-json -q
 echo "managing Dynamo DB Tables ..."
-python create_tables.py --mode create
+python3 create_tables.py --mode create
 
 # Create S3 bucket
 echo "Creating S3 buckets ..."
-python create_s3.py --mode create --bucket-prefix "anycompany-retail" --directory "data_files/anycompany-dataset" --s3-prefix "anycompany_profile"
+python3 create_s3.py --mode create --bucket-prefix "anycompany-retail" --directory "data_files/anycompany-dataset" --s3-prefix "anycompany_profile"
 
 
