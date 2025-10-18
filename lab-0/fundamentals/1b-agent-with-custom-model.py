90b34eb56e72a5284ff0268652276969337cbcd4
Rajesh S <rajesh.sripathi2@gmail.com>
2025-10-19 00:02:57 +0100

    Delete lab-0 directory

--- lab-0/fundamentals/1b-agent-with-custom-model.py
+++
@@ -1,5 +0,0 @@
-from strands import Agent
-
-agent = Agent(model="us.amazon.nova-premier-v1:0", system_prompt="You are a helpful assistant that provides concise answers. ")
-print(agent("Tell me about agentic AI"))
-
